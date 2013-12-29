c7226a8809dea590889d06444e48bf55355faef1
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2013-12-29 18:07:19 +0100

    teams retrieved from the database

--- routes/team.js
+++ routes/team.js
@@ -3,22 +3,23 @@
  */
 
 exports.list = function(req, res){
-	var teams = [
-		{
-			name: "Brasil",
-			points: 12,
-			wins: 2
-		},
-		{
-			name: "Spain",
-			points: 10,
-			wins: 1
-		},
-		{
-			name: "France",
-			points: 6,
-			wins: 3
-		}
-	]
-  	res.render('team', {title: 'List of teams', teams: teams});
+	var mongoClient = require('mongodb').MongoClient,
+	format = require('util').format;
+
+    mongoClient.connect('mongodb://127.0.0.1:27017/triliporra', function(err, db) {
+		if(err) throw err;
+
+		var teams = db.collection('teams');
+       
+		teams.count(function(err, count) {			
+			console.log(format("count = %s", count));
+		});
+
+		teams.find().toArray(function(err, results) {
+			console.dir(results);
+			db.close();
+
+			res.render('team', {title: 'List of teams', teams: results});
+		});
+    });
 };
--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -1,9 +1,53 @@
 describe("MongoDB", function() {
-    it("is there a server running", function(next) {
+    it("is there a server running", function() {
         var MongoClient = require('mongodb').MongoClient;
         MongoClient.connect('mongodb://127.0.0.1:27017/fastdelivery', function(err, db) {
             expect(err).toBe(null);
-            next();
+            db.close();            
         });
     });
+
+    it("there are teams in the database", function() {   	
+    	var MongoClient = require('mongodb').MongoClient
+    	, format = require('util').format;
+        MongoClient.connect('mongodb://127.0.0.1:27017/triliporra', function(err, db) {
+			if(err) throw err;
+
+			var teams = db.collection('teams');
+           
+			teams.count(function(err, count) {			
+				console.log(format("count = %s", count));
+			});
+
+			teams.find().toArray(function(err, results) {
+				console.dir(results);
+				db.close();
+			});
+        });
+    });
+
+    xit("insert test", function () {
+    	var MongoClient = require('mongodb').MongoClient
+	    , format = require('util').format;
+
+	  MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+	    if(err) throw err;
+
+	    var collection = db.collection('test_insert');
+	    collection.insert({a:2}, function(err, docs) {
+
+	      collection.count(function(err, count) {
+	        console.log(format("count = %s", count));
+	      });
+
+	      // Locate all the entries using find
+	      collection.find().toArray(function(err, results) {
+	        console.dir(results);
+	        // Let's close the db
+	        db.close();
+	      });
+	    });
+	  })
+    })
 });
+
