d4f074317cbb016caf57220bf9e2cf8864079dee
Asier Barrenetxea <asierba@gmail.com>
2014-01-02 16:35:11 +0100

    Dry-ed logic to retrieve data from database to a repository module

--- routes/team.js
+++ routes/team.js
@@ -1,20 +1,12 @@
+
 /*
  * GET teams listing.
  */
 
 exports.list = function(req, res){
-	var mongoClient = require('mongodb').MongoClient,
-		format = require('util').format,
-		MONGOHQ_URL = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/triliporra';
+	var repository = require('../repository');
 
-    mongoClient.connect(MONGOHQ_URL, function(err, db) {
-		if(err) throw err;
-
-		var teams = db.collection('teams');
-       
-		teams.find().toArray(function(err, results) {
-			db.close();
-			res.render('team/list', {title: 'List of teams', teams: results});
-		});
-    });
+	repository.getAll('teams', function (results) {			
+		res.render('team/list', {title: 'List of teams', teams: results});
+	});
 };
--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,10 @@
  */
 
 exports.list = function(req, res){
-    var mongoClient = require('mongodb').MongoClient,
-        format = require('util').format,
-        MONGOHQ_URL = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/triliporra';
+    var repository = require('../repository');
 
-    mongoClient.connect(MONGOHQ_URL, function(err, db) {
-        if(err) throw err;
-
-        var users = db.collection('users');
-       
-        users.find().toArray(function(err, results) {
-            db.close();
-            res.render('user/list', {title: 'List of users', users: results});
-        });
+    repository.getAll('users', function (results) {
+        res.render('user/list', {title: 'List of users', users: results});
     });
 };
 
