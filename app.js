c8fcc9cf16c91c1f7cadea7ef10611879fd31d01
Asier Barrenetxea <asierba@gmail.com>
2014-01-05 14:39:36 +0100

    User sign up added - still needs validation

--- app.js
+++ app.js
@@ -4,15 +4,21 @@
  */
 
 var express = require('express');
+var expressValidator = require('express-validator');
 var routes = require('./routes');
 var user = require('./routes/user');
 var team = require('./routes/team');
 var http = require('http');
 var path = require('path');
+var passport = require('passport');
+var passport_config = require('./passport-config');
 
 var app = express();
 
+
 // all environments
+app.use(express.bodyParser());
+app.use(expressValidator());
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,9 +35,22 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Routes
 app.get('/', routes.index);
+
 app.get('/users', user.list);
-app.get('/teams', team.list)
+app.get('/user/login', user.login);
+app.get('/user/signup', user.signup);
+app.post('/user/signup', user.signup_post);
+
+app.get('/teams', team.list);
+
+passport_config.init(passport);
+
+// Routes for passport
+app.get('/user/login-google', passport.authenticate('google'));
+app.get('/user/login-google-return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login-error' }));
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- repository.js
+++ repository.js
@@ -1,8 +1,8 @@
-exports.getAll = function (collection ,callback) {
-    var mongoClient = require('mongodb').MongoClient,
+var mongoClient = require('mongodb').MongoClient,
         format = require('util').format,
         MONGOHQ_URL = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/triliporra';
 
+exports.getAll = function (collection, callback) {
     mongoClient.connect(MONGOHQ_URL, function(err, db) {
         if(err) throw err;
 
@@ -13,4 +13,15 @@ exports.getAll = function (collection ,callback) {
             callback(results);
         });
     });
-}
+};
+
+exports.insert = function (collection, item, callback) {
+    mongoClient.connect(MONGOHQ_URL, function(err, db) {
+        if(err) throw err;
+        
+        db.collection(collection).insert(item, function() {           
+            db.close();
+            callback(item._id);
+        });        
+    });
+};
