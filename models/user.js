b71da5c510011fb689f4504297bada2dc271dc74
Asier Barrenetxea <asierba@gmail.com>
2014-01-05 18:39:22 +0100

    Mongo validation used instead of expressvalidator

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
     
 var userSchema = new mongoose.Schema({ 
-    name : { type : String, validate: /\S+/, index : { unique : true } },
-    points: { type: Number, default: 0, min: 0}
+    name : { type : String, validate: /\S+/, unique: true },
+    points: { type: Number, default: 0, min: 0},
+    email: { type: String, validate:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, unique: true},
+    password: { type: String, validate: /\S+/}
 });
 
 module.exports = mongoose.model('users', userSchema);
